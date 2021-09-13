1dc3dd3682125961425efc7181432cda42a1adc5
mrsteele <matt@omnionline.us>
2021-09-12 21:44:36 -0400

    technically works

--- src/pages/api/v2/test.js
+++ src/pages/api/v2/test.js
@@ -1,9 +1,12 @@
 import fs from 'fs/promises'
 import path from 'path'
+import raw from 'public/api/uniqueitems.json'
+// import raw from 'src/test'
+import { uniqueitems } from 'src/transforms/uniqueitems'
 
 
 export default async function handler(req, res) {
   // __dirname = /var/task/.next/server/pages/api/v2
-  const files = await fs.readdir(path.resolve(__dirname, '../../public'))
-  res.status(200).json({ __dirname, files })
+  // const files = await fs.readdir(path.resolve(__dirname, '../../public'))
+  res.status(200).json({ __dirname, raw })
 }
