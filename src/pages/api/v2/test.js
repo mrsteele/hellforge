d4b1cc503dde5a3aabbffa6aa94ec08bad81e2d2
mrsteele <matt@omnionline.us>
2021-09-12 21:34:23 -0400

    fix: still trying

--- src/pages/api/v2/test.js
+++ src/pages/api/v2/test.js
@@ -4,6 +4,6 @@ import path from 'path'
 
 export default async function handler(req, res) {
   // __dirname = /var/task/.next/server/pages/api/v2
-  const files = await fs.readdir(path.resolve(__dirname, '../../../public'))
+  const files = await fs.readdir(path.resolve(__dirname, '../../public'))
   res.status(200).json({ __dirname, files })
 }
