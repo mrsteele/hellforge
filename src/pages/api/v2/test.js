cf5a182fa06d710ecb96fe83d7394673238d0528
mrsteele <matt@omnionline.us>
2021-09-12 21:29:50 -0400

    fix: adding files check

--- src/pages/api/v2/test.js
+++ src/pages/api/v2/test.js
@@ -3,6 +3,7 @@ import path from 'path'
 
 
 export default async function handler(req, res) {
-  // const files = await fs.readdir(path.resolve(__dirname, '../../../../'))
-  res.status(200).json({ __dirname })
+  // __dirname = /var/task/.next/server/pages/api/v2
+  const files = await fs.readdir(path.resolve(__dirname, '../../../../../public'))
+  res.status(200).json({ __dirname, files })
 }
