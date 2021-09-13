2a64e40335adc1a8a4c2b799b8b406682f11eda7
mrsteele <matt@omnionline.us>
2021-09-12 21:51:20 -0400

    trying again

--- src/pages/api/v2/test.js
+++ src/pages/api/v2/test.js
@@ -1,11 +1,11 @@
 import fs from 'fs/promises'
 import path from 'path'
 // import raw from 'src/test'
-import { uniqueitems } from 'transforms/uniqueitems'
 
 
 export default async function handler(req, res) {
   // __dirname = /var/task/.next/server/pages/api/v2
-  const files = await fs.readdir(path.resolve(__dirname, '../../../../public'))
+  // const file = await fetch('/api/uniqueitems.json')
+  const files = await fs.readdir(path.resolve(__dirname, '../../../../../public/api'))
   res.status(200).json({ __dirname, files })
 }
