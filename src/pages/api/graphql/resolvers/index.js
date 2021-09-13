0b709f413f55cba68bff92bb05514e80a282ee88
mrsteele <matt@omnionline.us>
2021-09-13 13:30:27 -0400

    graphql totally working

--- src/pages/api/graphql/resolvers/index.js
+++ src/pages/api/graphql/resolvers/index.js
@@ -1,33 +1,22 @@
-// TODO: Move to environment variables
-const GITHUB_URL = 'https://api.github.com';
+import fetch from 'lib/fetch'
 
 export const resolvers = {
   Query: {
-    getUsers: async () => {
+    getUniques: async () => {
       try {
-        const users = await fetch(`${GITHUB_URL}/users`).then(res => res.json())
-        return users.map(({ id, login, avatar_url }) => ({
-          id,
-          login,
-          avatar_url,
-        }))
+        const data = await fetch(`/api/v2/items/unique`)
+        return data
       } catch (e) {
-        throw e;
+        throw e
       }
     },
-    getUser: async (_, args) => {
+    getUnique: async (_, args) => {
       try {
-        const {
-          data: {
-            id,
-            login,
-            avatar_url,
-          } = {},
-        } = await fetch(`${GITHUB_URL}/users/${args.name}`).then(res => res.json())
-        return { id, login, avatar_url };
+        const data = await fetch(`/api/v2/items/unique/${args.id}`)
+        return data
       } catch (e) {
-        throw e;
+        throw e
       }
-    },
-  },
-};
+    }
+  }
+}
--- src/pages/api/graphql/schemas/index.js
+++ src/pages/api/graphql/schemas/index.js
@@ -1,14 +1,45 @@
-import {gql} from 'apollo-server-micro';
+import {gql} from 'apollo-server-micro'
 
 export const typeDefs = gql`
-  type User {
+  type UniqueColors {
+    character: String
+    inventory: String
+  }
+
+  type UniqueGraphics {
+    ground: String
+    inventory: String
+  }
+
+  type UniqueSounds {
+    drop: String
+    dropframe: Int
+    use: String
+  }
+
+  type UniqueOther {
+    Rarity: String
+  }
+
+  type Unique {
     id: ID
-    login: String
-    avatar_url: String
+    name: String
+    availability: Int
+    availableOnLadder: Boolean
+    canDropMoreThanOncePerGame: Boolean
+    itemLvl: Int
+    requiredLevel: Int
+    type: String
+    playerCanHoldMoreThanOne: Boolean
+    price: Int
+    colors: UniqueColors
+    graphics: UniqueGraphics
+    sounds: UniqueSounds
+    other: UniqueOther
   }
   
   type Query {
-    getUsers: [User]
-    getUser(name: String!): User!
+    getUniques: [Unique]
+    getUnique(id: ID!): Unique!
   }
 `
