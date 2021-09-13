e7f07f3a94555c29d8363268d762fb107bbb6663
mrsteele <matt@omnionline.us>
2021-09-12 21:46:51 -0400

    adding 3 depth

--- src/pages/api/v2/test.js
+++ src/pages/api/v2/test.js
@@ -6,6 +6,6 @@ import { uniqueitems } from 'transforms/uniqueitems'
 
 export default async function handler(req, res) {
   // __dirname = /var/task/.next/server/pages/api/v2
-  const files = await fs.readdir(path.resolve(__dirname, '../../public'))
+  const files = await fs.readdir(path.resolve(__dirname, '../../../public'))
   res.status(200).json({ __dirname, files })
 }
