a53b35b7421cddbef4be65616b32e2ca8597c1b9
mrsteele <matt@omnionline.us>
2021-09-13 13:40:53 -0400

    fix: supporting graphiql in non-prod env

--- src/pages/api/graphql/index.js
+++ src/pages/api/graphql/index.js
@@ -2,7 +2,11 @@ import { ApolloServer } from 'apollo-server-micro'
 import { typeDefs } from './schemas'
 import { resolvers } from './resolvers'
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers })
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true // force graphiql
+})
 
 export const config = {
   api: {
